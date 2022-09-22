32f77c2c1a0b7059952e54dc031278c6fbda7001
LfernandesAcovisa <leonardo.fernandes@acovisa.com.br>
2022-09-22 16:56:50 -0300

    Modificação em inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,10 @@ import {
   CountdownContainer,
   FormContainer,
   HomeContainer,
+  MinuteAmountInput,
   Separator,
+  StartCountdownButton,
+  TaskInput,
 } from './styles'
 export function Home() {
   return (
@@ -11,11 +14,26 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
-          <span>durante</span>
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+          <datalist id="task-suggestions">
+            <option value="1454" />
+            <option value="2414" />
+            <option value="31414" />
+          </datalist>
+          "<span>durante</span>
           <label htmlFor="minutesAmount">durate</label>
-          <input type="number" id="minutosAmount" />
-
+          <MinuteAmountInput
+            type="number"
+            id="minutosAmount"
+            placeholder="00"
+            step={5}
+            min={0}
+            max={60}
+          />
           <span>minutos.</span>
         </FormContainer>
         <CountdownContainer>
@@ -25,11 +43,10 @@ export function Home() {
           <span>0</span>
           <span>0</span>
         </CountdownContainer>
-
-        <button type="submit">
+        <StartCountdownButton type="submit" disabled>
           <Play size={24} />
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -16,6 +16,7 @@ export const GlobalStyle = createGlobalStyle`
         background: ${(props) => props.theme['gray-900']};
         color: ${(props) => props.theme['gray-300']};
         -webkit-font-smoothing: antialiased;
+
     }
 
     border-style, input-security, textarea, button{

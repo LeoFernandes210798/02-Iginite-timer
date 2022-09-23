e2a3b74020e42fa907724002f077b345b3589d63
LfernandesAcovisa <leonardo.fernandes@acovisa.com.br>
2022-09-23 10:24:19 -0300

    Compnentes e status

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { Scroll, Timer } from 'phosphor-react'
 
 import { HeaderContainer } from './styles'
 
-import logoIgnite from '../assets/logo-ignite.svg'
+import logoIgnite from '../../assets/logo-ignite.svg'
 
 export function Header() {
   return (
